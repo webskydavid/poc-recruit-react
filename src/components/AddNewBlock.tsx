7e4ccea95cacca46f42249ceac89719fd64dd8d3
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2021-03-05 13:58:58 +0100

    feat: Style form

--- src/components/AddNewBlock.tsx
+++ src/components/AddNewBlock.tsx
@@ -40,10 +40,8 @@ const AddNewBlock: FC = () => {
         {({ values, ...rest }) => {
           return (
             <Form>
+              <h3 className="form__header">Rekruter</h3>
               <div className="form__set">
-                <div className="form__field">
-                  <h3>eofjeof</h3>
-                </div>
                 <div className="form__field">
                   <label htmlFor="name">Imię i nazwisko</label>
                   <Field name="recruter.name" />
@@ -52,57 +50,60 @@ const AddNewBlock: FC = () => {
                   <label htmlFor="name">Kontakt</label>
                   <Field name="recruter.contact" />
                 </div>
-                <FieldArray name="recruter.steps">
-                  {({ push, remove }) => {
-                    const fields = values.recruter.steps.map((step, index) => (
-                      <>
-                        <div className="form__field">
-                          <label htmlFor="name">
-                            Typ{' '}
-                            <button type="button" onClick={() => remove(index)}>
-                              X
-                            </button>
-                          </label>
-                          <Field name={`recruter.steps.${index}.type`} />
-                        </div>
-                        <div className="form__field">
-                          <label htmlFor="name">Kiedy</label>
-                          <Field name={`recruter.steps.${index}.when`} />
-                        </div>
-                      </>
-                    ));
-
-                    return (
-                      <>
-                        <div className="form__field">
-                          <button
-                            type="button"
-                            onClick={() => push({ type: '', when: '' })}
-                          >
-                            Add step
-                          </button>
-                        </div>
-                        {fields}
-                      </>
-                    );
-                  }}
-                </FieldArray>
               </div>
 
+              <FieldArray name="recruter.steps">
+                {({ push, remove }) => {
+                  const fields = values.recruter.steps.map((step, index) => (
+                    <div className="form__set">
+                      <div className="form__field">
+                        <label htmlFor="name">Typ</label>
+                        <Field name={`recruter.steps.${index}.type`} />
+                      </div>
+                      <div className="form__field">
+                        <label htmlFor="name">Kiedy</label>
+                        <Field name={`recruter.steps.${index}.when`} />
+                      </div>
+                      <div className="form__field">
+                        <button type="button" onClick={() => remove(index)}>
+                          X
+                        </button>
+                      </div>
+                    </div>
+                  ));
+
+                  return (
+                    <>
+                      <h3 className="form__header">
+                        Kroki{' '}
+                        <button
+                          type="button"
+                          onClick={() => push({ type: '', when: '' })}
+                        >
+                          Dodaj{' '}
+                        </button>
+                      </h3>
+                      {fields}
+                    </>
+                  );
+                }}
+              </FieldArray>
+
+              <h3 className="form__header">Firma</h3>
               <div className="form__set">
-                <div>
+                <div className="form__field">
                   <label htmlFor="name">Firma</label>
                   <Field name="company.name" />
                 </div>
-                <div>
+                <div className="form__field">
                   <label htmlFor="name">Kontakt</label>
                   <Field name="company.contact" />
                 </div>
-                <div>
+                <div className="form__field">
                   <label htmlFor="name">Link to oferty</label>
                   <Field name="company.offer_link" />
                 </div>
-                <div>
+                <div className="form__field">
                   <label htmlFor="name">Strona firmy</label>
                   <Field name="company.website" />
                 </div>
@@ -111,22 +112,32 @@ const AddNewBlock: FC = () => {
               <FieldArray name="tech_stack">
                 {({ push, remove }) => {
                   return (
-                    <div className="form__set">
-                      <button type="button" onClick={() => push('')}>
-                        Add tech
-                      </button>
-                      {values.tech_stack.map((tech, index) => (
-                        <>
-                          <div>
-                            <label htmlFor="name">Stack</label>
-                            <Field name={`tech_stack.${index}`} />
-                          </div>
-                          <button type="button" onClick={() => remove(index)}>
-                            X
-                          </button>
-                        </>
-                      ))}
-                    </div>
+                    <>
+                      <h3 className="form__header">
+                        Stack technologiczny{' '}
+                        <button type="button" onClick={() => push('')}>
+                          Dodaj
+                        </button>
+                      </h3>
+                      <div className="form__set">
+                        {values.tech_stack.map((tech, index) => (
+                          <>
+                            <div className="form__field">
+                              <label htmlFor="name">Stack</label>
+                              <Field name={`tech_stack.${index}`} />
+                            </div>
+                            <div className="form__field">
+                              <button
+                                type="button"
+                                onClick={() => remove(index)}
+                              >
+                                X
+                              </button>
+                            </div>
+                          </>
+                        ))}
+                      </div>
+                    </>
                   );
                 }}
               </FieldArray>
@@ -134,33 +145,43 @@ const AddNewBlock: FC = () => {
               <FieldArray name="salary">
                 {({ push, remove }) => {
                   return (
-                    <div className="form__set">
-                      <button
-                        type="button"
-                        onClick={() => push({ from: 0, to: 0, type: '' })}
-                      >
-                        Add salary
-                      </button>
-                      {values.salary.map((salary, index) => (
-                        <>
-                          <div>
-                            <label htmlFor="name">Od</label>
-                            <Field name={`salary.${index}.from`} />
-                          </div>
-                          <div>
-                            <label htmlFor="name">Do</label>
-                            <Field name={`salary.${index}.to`} />
-                          </div>
-                          <div>
-                            <label htmlFor="name">Typ</label>
-                            <Field name={`salary.${index}.type`} />
-                          </div>
-                          <button type="button" onClick={() => remove(index)}>
-                            X
-                          </button>
-                        </>
-                      ))}
-                    </div>
+                    <>
+                      <h3 className="form__header">
+                        Widełki płacowe{' '}
+                        <button
+                          type="button"
+                          onClick={() => push({ from: 0, to: 0, type: '' })}
+                        >
+                          Dodaj
+                        </button>
+                      </h3>
+                      <div className="form__set">
+                        {values.salary.map((salary, index) => (
+                          <>
+                            <div className="form__field">
+                              <label htmlFor="name">Od</label>
+                              <Field name={`salary.${index}.from`} />
+                            </div>
+                            <div className="form__field">
+                              <label htmlFor="name">Do</label>
+                              <Field name={`salary.${index}.to`} />
+                            </div>
+                            <div className="form__field">
+                              <label htmlFor="name">Typ</label>
+                              <Field name={`salary.${index}.type`} />
+                            </div>
+                            <div className="form__field">
+                              <button
+                                type="button"
+                                onClick={() => remove(index)}
+                              >
+                                X
+                              </button>
+                            </div>
+                          </>
+                        ))}
+                      </div>
+                    </>
                   );
                 }}
               </FieldArray>
@@ -168,23 +189,35 @@ const AddNewBlock: FC = () => {
               <FieldArray name="benefits">
                 {({ push, remove }) => {
                   return (
-                    <div className="form__set">
-                      <button type="button" onClick={() => push({ name: '' })}>
-                        Add salary
-                      </button>
-                      {values.benefits.map((benefit, index) => (
-                        <>
-                          <div>
-                            <label htmlFor="name">Nazwa</label>
-                            <Field name={`benefits.${index}.name`} />
-                          </div>
-
-                          <button type="button" onClick={() => remove(index)}>
-                            X
-                          </button>
-                        </>
-                      ))}
-                    </div>
+                    <>
+                      <h3 className="form__header">
+                        Benefity{' '}
+                        <button
+                          type="button"
+                          onClick={() => push({ name: '' })}
+                        >
+                          Dodaj
+                        </button>
+                      </h3>
+                      <div className="form__set">
+                        {values.benefits.map((benefit, index) => (
+                          <>
+                            <div className="form__field">
+                              <label htmlFor="name">Nazwa</label>
+                              <Field name={`benefits.${index}.name`} />
+                            </div>
+                            <div className="form__field">
+                              <button
+                                type="button"
+                                onClick={() => remove(index)}
+                              >
+                                X
+                              </button>
+                            </div>
+                          </>
+                        ))}
+                      </div>
+                    </>
                   );
                 }}
               </FieldArray>
